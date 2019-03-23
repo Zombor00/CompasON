b3e5ac49125cd1b7f8e2ee0783c2e6d09eeb4e0c
antonio <antonio@antonio-Inspiron-One-2320>
2019-03-23 15:45:32 +0100

    Antonio 23 de marzo. 15:45

---
+++ src/excepciones/ExcepcionLoginErrorCredenciales.java
@@ -0,0 +1,12 @@
+package excepciones;
+
+/**
+ * Esta excepcion se lanza cuando el nombre y/o la contrasenia utilizados
+ * al hacer un login son incorrectos
+ *
+ * @author Antonio Garcia antonio.garcian@estudiante.uam.es
+ * @version 1.0 (05-03-2019)
+ */
+public class ExcepcionLoginErrorCredenciales extends Exception {
+
+}
---
+++ src/test/Simulacion.java
@@ -0,0 +1,13 @@
+package test;
+
+/**
+ * Implementamos la clase Aplicacion utilizando el Patron Singleton
+ *
+ * @author Antonio Garcia antonio.garcian@estudiante.uam.es
+ * @version 1.0 (23-03-2019)
+ */
+public class Simulacion {
+	public static void main() {
+		System.out.println("Simulamos el funcionamiento de la aplicacion.");
+	}
+}
